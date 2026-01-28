1a37307a851f742f4343ea583b42cc120290804b
Phitsinee <semmisempai@gmail.com>
2026-01-28 09:15:07 +0700

    CardCounter.java

--- progmeth_Lab1/src/main/java/logic/CardCounter.java
+++ progmeth_Lab1/src/main/java/logic/CardCounter.java
@@ -1,12 +1,32 @@
 package logic;
 
 public class CardCounter {
-	
 	private UnitCard card;
 	private int count;
-	
-	public String toString() {
-		return  this.getCard() + " x " + this.getCount();
+
+	public CardCounter(UnitCard card, int count) {
+		this.card = card;
+		setCount(count);
 	}
 
+	public void setCount(int count) {
+		this.count = Math.max(0, count);
+	}
+
+	public UnitCard getCard() {
+		return card;
+	}
+
+	public void setCard(UnitCard card) {
+		this.card = card;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	@Override
+	public String toString() {
+		return card.toString() + " x " + count;
+	}
 }
