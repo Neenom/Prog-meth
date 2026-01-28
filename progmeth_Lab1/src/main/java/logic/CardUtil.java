50e32a7a3811fb553fccf5484983d40bac6fd734
Phitsinee <semmisempai@gmail.com>
2026-01-28 09:13:56 +0700

    CardUtil.java

--- progmeth_Lab1/src/main/java/logic/CardUtil.java
+++ progmeth_Lab1/src/main/java/logic/CardUtil.java
@@ -1,81 +1,89 @@
 package logic;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CardUtil {
-	
+
 	public static boolean isExistsInList(UnitCard card, ArrayList<UnitCard> list) {
-		
-		//TODO: Fill Code
+		for (UnitCard c : list) {
+			if (c.equals(card)) return true;
+		}
 		return false;
-
 	}
-	
+
 	public static boolean isExistsInList(UnitDeck deck, ArrayList<UnitDeck> list) {
-		
-		//TODO: Fill Code
+		for (UnitDeck d : list) {
+			if (d.equals(deck)) return true;
+		}
 		return false;
-
 	}
-	
+
 	public static boolean cardExistsInDeckList(ArrayList<UnitDeck> deckList, UnitCard cardToTest) {
-		
-		//TODO: Fill Code
+		for (UnitDeck deck : deckList) {
+			if (deck.existsInDeck(cardToTest)) {
+				return true;
+			}
+		}
 		return false;
 	}
-	
-	public static ArrayList<UnitCard> getCardsFromFile(String filename){
-		
-		ArrayList<UnitCard> cardsFromFile = new ArrayList<UnitCard>();
-		InputStream inputStream = CardUtil.class.getClassLoader().getResourceAsStream(filename);
-          	if (inputStream == null) {
-                	System.out.println("Cannot find file!");
-                	return null;
-            	}
 
-            	Scanner myReader = new Scanner(inputStream);
+	public static ArrayList<UnitCard> getCardsFromFile(String filename) {
+		ArrayList<UnitCard> cardsFromFile = new ArrayList<>();
 
+		try {
+			Scanner sc = new Scanner(new File(filename));
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+				String[] parts = line.split(",");
 
-		//TODO: Fill Code below
-		
-		return null;
+				if (parts.length != 5) {
+					System.out.println("File contains string with incorrect format!");
+					sc.close();
+					return null;
+				}
+
+				UnitCard card = new UnitCard(
+						parts[0].trim(),
+						Integer.parseInt(parts[1].trim()),
+						Integer.parseInt(parts[2].trim()),
+						Integer.parseInt(parts[3].trim()),
+						parts[4].trim()
+				);
+				cardsFromFile.add(card);
+			}
+			sc.close();
+		} catch (Exception e) {
+			System.out.println("File contains string with incorrect format!");
+			return null;
+		}
+
+		return cardsFromFile;
 	}
 
 	public static void printCardList(ArrayList<UnitCard> cardList, boolean verbose) {
-		
-		for(int i = 0; i < cardList.size(); i++) {
-			System.out.println(i + ") " + cardList.get(i));
-			if(verbose) {
-				System.out.println("Blood Cost: " + cardList.get(i).getBloodCost());
-				System.out.println(cardList.get(i).getFlavorText());
-				if(i < cardList.size()-1) System.out.println("-----");
+		for (UnitCard card : cardList) {
+			System.out.println(card);
+			System.out.println("Blood Cost: " + card.getBloodCost());
+			if (verbose) {
+				System.out.println(card.getFlavorText());
 			}
 		}
 	}
-	
+
 	public static void printDeck(UnitDeck ud) {
-		
-		if(ud.getCardsInDeck().size() == 0) {
-			System.out.println("EMPTY DECK");
-		}else {
-			for(CardCounter cc : ud.getCardsInDeck()) {
-				System.out.println(cc);
-			}
+		for (CardCounter cc : ud.getCardsInDeck()) {
+			System.out.println(cc);
 		}
-		
 		System.out.println("Total Cards: " + ud.cardCount());
 	}
-	
+
 	public static void printDeckList(ArrayList<UnitDeck> deckList) {
-		
-		
-		for(int i = 0; i < deckList.size(); i++) {
-			System.out.println(i + ") " + deckList.get(i).getDeckName());
-			printDeck(deckList.get(i));
-			if(i < deckList.size()-1) System.out.println("-----");
+		for (UnitDeck deck : deckList) {
+			System.out.println(deck.getDeckName());
+			printDeck(deck);
+			System.out.println("-----");
 		}
 	}
 }
